53bb6e0dc4af6d6f3be0eec9dfcb8fe051b1757e
OmarAdelGalal <omaradel123232432@gmail.com>
2026-01-07 16:53:39 +0200

    first commit

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="theme-color" content="#111827" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/icons/apple-touch-icon.svg" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+    </>
+  );
+}
